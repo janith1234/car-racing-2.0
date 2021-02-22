426d7ce7d32e230922016932b687fb411f4c3b0e
janith <vajanith@gmail.com>
2021-02-22 15:07:34 +0530

    changed

--- js/Player.js
+++ js/Player.js
@@ -1,34 +1,34 @@
 class Player {
-  constructor(){
+  constructor() {
     this.index = null;
     this.distance = 0;
     this.name = null;
   }
 
-  getCount(){
+  getCount() {
     var playerCountRef = database.ref('playerCount');
-    playerCountRef.on("value",(data)=>{
+    playerCountRef.on("value", (data) => {
       playerCount = data.val();
     })
   }
 
-  updateCount(count){
+  updateCount(count) {
     database.ref('/').update({
       playerCount: count
     });
   }
 
-  update(){
+  update() {
     var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).set({
-      name:this.name,
-      distance:this.distance
+      name: this.name,
+      distance: this.distance
     });
   }
 
-  static getPlayerInfo(){
+  static getPlayerInfo() {
     var playerInfoRef = database.ref('players');
-    playerInfoRef.on("value",(data)=>{
+    playerInfoRef.on("value", (data) => {
       allPlayers = data.val();
     })
   }
